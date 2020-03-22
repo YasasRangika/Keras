5e8feca7152499e9372a94c3928de92ad2d76438
yasasrangika <rangikaymendis@gmail.com>
2020-03-22 22:40:00 +0530

    duplicate entries

--- duplicates.py
+++ duplicates.py
@@ -9,15 +9,20 @@ df = pd.read_csv('5.162.125.116.csv')
 
 # df.drop_duplicates(['http_method'], keep='last')
 
-# df.drop_duplicates(subset="timestamp", keep="last", inplace=True)
-
+df.drop_duplicates(subset="timestamp", keep="first", inplace=True)
+print(df)
 # ids = df["timestamp"]
-# duplicates = df[ids.isin(ids[ids.duplicated()])].sort_values("timestamp")
+# duplicates = df[ids.isin(ids[ids.duplicated()])].sort_values("http_method")
+# print(duplicates.head(30))
 
 # Better peformance-remove comparing two columns, but when it has two columns to compare with GET and POST for example
 # it will not compare.. Find a way to compare and keep only POSTs and DELETEs
-duplicates = df.reset_index().drop_duplicates(subset=['timestamp', 'http_method'], keep="first").set_index('timestamp')
-print(duplicates.head(30))
+# duplicates = df.reset_index().drop_duplicates(subset=['timestamp', 'http_method'], keep="first").set_index('timestamp')
+# print(duplicates[['http_method']])
+
+# duplicate_in_time = duplicates.duplicated(subset=['timestamp'])
+# if duplicate_in_time.any():
+#     print(duplicates.loc[duplicate_in_time])
 # duplicates = df[df.duplicated(keep=False)]
 
 # duplicates = df[df['timestamp'].duplicated() == True]
